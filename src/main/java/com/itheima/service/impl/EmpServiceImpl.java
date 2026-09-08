623f31d4c644a4d3252be0035bb584d6b74be3fe
Zhao Wei Hang <yaogui268@qq.com>
2026-09-08 21:40:59 +0800

    Jwt令牌设计

--- src/main/java/com/itheima/service/impl/EmpServiceImpl.java
+++ src/main/java/com/itheima/service/impl/EmpServiceImpl.java
@@ -7,6 +7,7 @@ import com.itheima.mapper.EmpMapper;
 import com.itheima.pojo.*;
 
 import com.itheima.service.EmpService;
+import com.itheima.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,10 @@ import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @Service
 public class EmpServiceImpl implements EmpService {
@@ -31,7 +35,11 @@ public class EmpServiceImpl implements EmpService {
         Emp e = empMapper.selectByUsernameAndPassword(emp);
         if (e != null) {
             log.info("登陆成功,员工信息:{}",e);
-            return new LoginInfo(e.getId(), e.getUsername(), e.getName(),"");
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id",e.getId());
+            claims.put("username",e.getUsername());
+            String jwt = JwtUtils.generateJwt(claims);
+            return new LoginInfo(e.getId(), e.getUsername(), e.getName(),jwt);
         }
         return null;
     }
